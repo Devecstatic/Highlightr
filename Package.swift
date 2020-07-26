78b15884c5b2d3c6f742cd0dee5ebe483750554e
Devecstatic <ethanlipnik@gmail.com>
2020-07-25 21:27:33 -0400

    Initial SPM

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Highlightr",
+    platforms: [
+        .macOS(.v10_11),
+        .iOS(.v8),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,7 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Highlightr",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .copy("Assets"),
+                .copy("Highlightr")
+            ]),
         .testTarget(
             name: "HighlightrTests",
             dependencies: ["Highlightr"]),
